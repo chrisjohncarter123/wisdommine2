3f2f74cc4f41d3528202ed433ec735e9fd49b4ba
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-17 03:51:07 +0000

    first commit

---
+++ wisdommine2/lib/wisdommine2.rb
@@ -0,0 +1,6 @@
+require "wisdommine2/version"
+
+module Wisdommine2
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ wisdommine2/lib/wisdommine2/version.rb
@@ -0,0 +1,3 @@
+module Wisdommine2
+  VERSION = "0.1.0"
+end
